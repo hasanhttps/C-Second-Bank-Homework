ba0bb7e0a1e7cf7aa381d23fe48dc4eff913d4ef
hasanhttps <hasanabdullazad@gmail.com>
2023-04-26 23:55:43 +0400

    Bank

---
+++ C# Bank Homework 2/models/Operation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Bank_Homework_2.models
+{
+    internal class Operation
+    {
+    }
+}
